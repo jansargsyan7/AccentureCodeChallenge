03a1cac938c3e3885d724b41efc9e259c696bd54
Jan Sargsyan <jan@letscodeit.co.uk>
2022-11-04 23:12:16 +0000

    add: basic project structure and quick fetch

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { API_ENDPOINT } from './src/constants';
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [products, setProducts] = useState({});
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(API_ENDPOINT)
+        .then((response) => response.json())
+        .then((json) => {
+          const productsRaw = json;
+          productsRaw.sort((a, b) => (a.order > b.order ? 1 : -1));
+          setProducts(productsRaw);
+        })
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+    }, 2000);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {isLoading && <Text>Loading</Text>}
+      {!isLoading &&
+        products.map((product) => (
+          <View style={styles.product}>
+            <Text key={product.id}>{product.title}</Text>
+          </View>
+        ))}
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +41,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  product: {
+    backgroundColor: '#c9c9c9',
+    marginTop: 10,
+    padding: 10,
+    paddingHorizontal: 30,
   },
 });
---
+++ src/constants/index.js
@@ -0,0 +1,1 @@
+export const API_ENDPOINT = 'https://demo1087320.mockable.io/products';
