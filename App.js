84f64037b06f684d26ab391a3d1e35e90395d7fd
Jan Sargsyan <jan@letscodeit.co.uk>
2022-11-05 14:13:51 +0000

    chore: move home to screen

--- App.js
+++ App.js
@@ -1,115 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, Image, Animated, Easing } from 'react-native';
-import React, { useEffect, useState, useRef } from 'react';
-import { API_ENDPOINT } from './src/constants';
-import { LogBox } from 'react-native';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Product from './src/screens/Product';
 import HomeScreen from './src/screens/HomeScreen';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
-  const [rotateAnimation, setRotateAnimation] = useState(new Animated.Value(0));
-
-  const [isLoading, setLoading] = useState(true);
-  const [products, setProducts] = useState({});
-
-  LogBox.ignoreLogs([
-    "No native splash screen registered for given view controller. Call 'SplashScreen.show' for given view controller first.",
-  ]);
-
   const Stack = createNativeStackNavigator();
 
-  useEffect(() => {
-    setTimeout(() => {
-      fetch(API_ENDPOINT)
-        .then((response) => response.json())
-        .then((json) => {
-          const productsRaw = json;
-          productsRaw.sort((a, b) => (a.order > b.order ? 1 : -1));
-          setProducts(productsRaw);
-        })
-        .catch((error) => console.error(error))
-        .finally(() => setLoading(false));
-    }, 2000);
-  }, []);
-
-  useEffect(() => {
-    console.log(products[0]);
-  }, [products]);
-
-  useEffect(() => {
-    Animated.loop(
-      Animated.timing(rotateAnimation, {
-        toValue: 1,
-        duration: 1000,
-        easing: Easing.linear,
-        useNativeDriver: true,
-      })
-    ).start(() => {
-      rotateAnimation.setValue(0);
-    });
-  }, []);
-
-  const interpolateRotating = rotateAnimation.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['0deg', '360deg'],
-  });
-
-  const animatedStyle = {
-    transform: [
-      {
-        rotate: interpolateRotating,
-      },
-    ],
-  };
-
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Welcome' }} />
-        <Stack.Screen name="Product" component={Product} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Welcome' }} />
+          <Stack.Screen name="Product" component={Product} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      <StatusBar style="auto" />
+    </>
   );
 }
-
-// <View style={styles.container}>
-//   {isLoading && (
-//     <Animated.View style={animatedStyle}>
-//       <Image style={styles.spinner} source={require('./src/assets/spinner.png')} />
-//     </Animated.View>
-//   )}
-//   {!isLoading &&
-//     products.map((product) => (
-//       <TouchableOpacity
-//         key={product.id}
-//         style={styles.product}
-//         onPress={() => {
-//           alert('You tapped the button!');
-//         }}
-//       >
-//         <Text key={product.id}>{product.title}</Text>
-//       </TouchableOpacity>
-//     ))}
-//   <StatusBar style="auto" />
-// </View>;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  product: {
-    backgroundColor: '#c9c9c9',
-    marginTop: 10,
-    padding: 10,
-    paddingHorizontal: 30,
-  },
-  spinner: {
-    width: 50,
-    height: 50,
-  },
-});
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,104 @@
-import React from 'react';
-import { TouchableOpacity, Text } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image, Animated, Easing } from 'react-native';
+import { LogBox } from 'react-native';
+import { API_ENDPOINT } from '../constants';
 
 const HomeScreen = ({ navigation }) => {
+  const [rotateAnimation, setRotateAnimation] = useState(new Animated.Value(0));
+
+  const [isLoading, setLoading] = useState(true);
+  const [products, setProducts] = useState({});
+
+  LogBox.ignoreLogs([
+    "No native splash screen registered for given view controller. Call 'SplashScreen.show' for given view controller first.",
+  ]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(API_ENDPOINT)
+        .then((response) => response.json())
+        .then((json) => {
+          const productsRaw = json;
+          productsRaw.sort((a, b) => (a.order > b.order ? 1 : -1));
+          setProducts(productsRaw);
+        })
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+    }, 2000);
+  }, []);
+
+  useEffect(() => {
+    console.log(products[0]);
+  }, [products]);
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.timing(rotateAnimation, {
+        toValue: 1,
+        duration: 1000,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      })
+    ).start(() => {
+      rotateAnimation.setValue(0);
+    });
+  }, []);
+
+  const interpolateRotating = rotateAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
+
+  const animatedStyle = {
+    transform: [
+      {
+        rotate: interpolateRotating,
+      },
+    ],
+  };
+
   return (
-    <TouchableOpacity onPress={() => navigation.navigate('Product', { id: '111' })}>
-      <Text>Go to profile</Text>
-    </TouchableOpacity>
+    <View style={styles.container}>
+      {isLoading && (
+        <Animated.View style={animatedStyle}>
+          <Image style={styles.spinner} source={require('../assets/spinner.png')} />
+        </Animated.View>
+      )}
+      {!isLoading &&
+        products.map((product) => (
+          <TouchableOpacity
+            key={product.id}
+            style={styles.product}
+            onPress={() => navigation.navigate('Product', { id: product.id })}
+          >
+            <Text key={product.id}>{product.title}</Text>
+          </TouchableOpacity>
+        ))}
+    </View>
   );
 };
+
+// <TouchableOpacity onPress={() => navigation.navigate('Product', { id: '111' })}>
+//   <Text>Go to profile</Text>
+// </TouchableOpacity>
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  product: {
+    backgroundColor: '#c9c9c9',
+    marginTop: 10,
+    padding: 10,
+    paddingHorizontal: 30,
+  },
+  spinner: {
+    width: 50,
+    height: 50,
+  },
+});
 
 export default HomeScreen;
