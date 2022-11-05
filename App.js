04489d0c8248619aab25e21fa15891c73163c362
Jan Sargsyan <jan@letscodeit.co.uk>
2022-11-05 13:41:33 +0000

    feat: animated spinner

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image, Animated, Easing } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
 import { API_ENDPOINT } from './src/constants';
+import { LogBox } from 'react-native';
 
 export default function App() {
+  const [rotateAnimation, setRotateAnimation] = useState(new Animated.Value(0));
+
   const [isLoading, setLoading] = useState(true);
   const [products, setProducts] = useState({});
+
+  LogBox.ignoreLogs([
+    "No native splash screen registered for given view controller. Call 'SplashScreen.show' for given view controller first.",
+  ]);
 
   useEffect(() => {
     setTimeout(() => {
@@ -21,14 +28,54 @@ export default function App() {
     }, 2000);
   }, []);
 
+  useEffect(() => {
+    console.log(products[0]);
+  }, [products]);
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.timing(rotateAnimation, {
+        toValue: 1,
+        duration: 1000,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      })
+    ).start(() => {
+      rotateAnimation.setValue(0);
+    });
+  }, []);
+
+  const interpolateRotating = rotateAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
+
+  const animatedStyle = {
+    transform: [
+      {
+        rotate: interpolateRotating,
+      },
+    ],
+  };
+
   return (
     <View style={styles.container}>
-      {isLoading && <Text>Loading</Text>}
+      {isLoading && (
+        <Animated.View style={animatedStyle}>
+          <Image style={styles.spinner} source={require('./src/assets/spinner.png')} />
+        </Animated.View>
+      )}
       {!isLoading &&
         products.map((product) => (
-          <View style={styles.product}>
+          <TouchableOpacity
+            key={product.id}
+            style={styles.product}
+            onPress={() => {
+              alert('You tapped the button!');
+            }}
+          >
             <Text key={product.id}>{product.title}</Text>
-          </View>
+          </TouchableOpacity>
         ))}
       <StatusBar style="auto" />
     </View>
@@ -47,5 +94,9 @@ const styles = StyleSheet.create({
     marginTop: 10,
     padding: 10,
     paddingHorizontal: 30,
+  },
+  spinner: {
+    width: 50,
+    height: 50,
   },
 });
